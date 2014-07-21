7d6273290055ce686a1de8bd04cccb4fc96d539d
Tellechea <arthurtellechea@hotmail.com>
2014-07-21 15:16:49 -0300

    Projeto estruturado

--- Biblioteca/src/br/ifrn/tads/poo/biblioteca/acervo/itemAcervo.java
+++ Biblioteca/src/br/ifrn/tads/poo/biblioteca/acervo/itemAcervo.java
@@ -10,9 +10,6 @@ public class itemAcervo {
 	protected Date dataAluguel , dataDevolucao;
 	protected String codigoitem;
 	protected boolean pago;
-        protected int quantidade;
-        Aluga aluguel[] = new Aluga[2];
-        private int qnt = 0;
 	
 	itemAcervo(){
 	}
@@ -35,25 +32,11 @@ public class itemAcervo {
 	
 	
 	void alugar(){
-            if (pago == true){
-               if(quantidade >= 1){
-                quantidade -= 1;
-                Aluga aluguel = new Aluga();
-               }
-               else{
-                  System.out.println("Sem unidades no acervo.");
-               }
-            }     
-            else{
-                System.out.println("Há um débito anterior nesse registro, aluguel não autorizado.");
-            }
+           
 	}
 	
 	double devolver(){
-            if ( aluguel[].getAlugado() == true ){
-                
-            }
-            
+              
 		return custo;
 		
 	}
