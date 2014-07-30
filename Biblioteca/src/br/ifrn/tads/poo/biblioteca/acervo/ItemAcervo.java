6e1ffd05d64a8cd8ec454819649bcd2445f4214d
unknown <20132014040429@CNAT111830.ifrn.local>
2014-07-30 11:33:04 -0300

    Implementando metodo Aluga e comentando lógica de devolução

--- Biblioteca/src/br/ifrn/tads/poo/biblioteca/acervo/ItemAcervo.java
+++ Biblioteca/src/br/ifrn/tads/poo/biblioteca/acervo/ItemAcervo.java
@@ -1,6 +1,9 @@
 package br.ifrn.tads.poo.biblioteca.acervo;
+import java.util.Calendar;
 import java.util.Date;
 import br.ifrn.tads.poo.biblioteca.usuario.Usuario;
+
+import java.text.SimpleDateFormat;
 import java.time.Clock;
 
 
@@ -31,10 +34,34 @@ public class ItemAcervo {
 	
 	
 	void alugar(){
+		
+		
+		Date data1 = new Date();
+		
+		SimpleDateFormat formatDataAl = new SimpleDateFormat("yyyy-MM-dd");
+		dataAluguel = data1;
+		
+		Calendar calendar= Calendar.getInstance();
+		calendar.setTime(data1);
+		calendar.add(Calendar.DAY_OF_MONTH, 10);
+		Date data2 = calendar.getTime();
+		SimpleDateFormat formatDataDev = new SimpleDateFormat("yyyy-MM-dd");
+		dataDevolucao = data2;
+		
            
 	}
 	
 	double devolver(){
+		
+		// se dataDoDia <= dataDevolucao && pago == true {
+		// }
+		// se dataDoDia <= dataDevolucao && pago == false{
+		// }
+		// se dataDoDia >= dataDevolucao && pago == false{
+		// }
+		// se dataDoDia >= dataDevolucao && pago == true{
+		// }
+		
               
 		return custo;
 		
